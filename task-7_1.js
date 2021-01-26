4c3ba43c0adea5f051898e13f96d142edb7adfcf
Jane-Horbul <es.horbul@gmail.com>
2021-01-27 00:01:31 +0200

    refined task-7_1

--- task-7_1.js
+++ task-7_1.js
@@ -1,6 +1,17 @@
 const elemRef = document.querySelector('.item');
+const itemTitleref = document.querySelector('h2');
+const listRef = document.querySelector('#categories');
+
 
 console.log(`В списке ${document.getElementsByClassName('item').length} категории`);
+
+const itemChildrenArray = Array.from(document.getElementsByClassName('item'));
+
+for (let num = 0; num <= itemChildrenArray.length; num++) {
+
+    console.log(`Категория: ${itemChildrenArray[num].firstElementChild.textContent} 
+Количество элементов: ${itemChildrenArray[num].lastElementChild.children.length}`);
+};
 
 
 
